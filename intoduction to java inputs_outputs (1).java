506e8f1c74b47abc05ed247b99dd61b2bf4a0885
divyatirupati <divyatirupati2@gmail.com>
2024-09-30 19:28:29 +0530

    Add files via upload

---
+++ intoduction to java inputs_outputs (1).java
@@ -0,0 +1,52 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("45");
+    System.out.println(34+4);
+    System.out.println("94+7");
+    System.out.println("5*10");
+    System.out.println(5*10);
+    System.out.println(4*9);
+    System.out.println("hello"+" " +"1");
+    System.out.println("Hello"+" "+"10");
+    Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+    /*
+		 -2* 10^9  < int < 2* 10^9
+		 -9* 10^18 < long < 9* 10^18
+		  */
+
+    System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+
+     Scanner s = new Scanner(System.in);
+		 String str = s.nextLine();
+		 System.out.println(str);
+
+
+    int a = 20;
+		int b = 10;
+		System.out.print(b);
+
+
+
+
+
+
+
+
+     
+      System.out.println("Hello, World!");
+  }
+}
