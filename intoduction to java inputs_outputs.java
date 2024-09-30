6becaa7988d6afcdd93d67bb3086a633a8e09f70
divyatirupati <divyatirupati2@gmail.com>
2024-09-30 19:31:04 +0530

    Add files via upload

---
+++ intoduction to java inputs_outputs.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("45");
+    System.out.println(34+4);
+    System.out.println("94+7");
+    System.out.println("5*10");
+    System.out.println(5*10);
+    System.out.println(4*9);
+    System.out.println("hello"+" " +"1");
+    System.out.println("Hello"+" "+"10");
+
+
+
+
+     
+      System.out.println("Hello, World!");
+  }
+}
