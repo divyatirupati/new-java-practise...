33921a9990de49c0a499b16fcadd868f45985fe3
divyatirupati <divyatirupati2@gmail.com>
2024-10-01 19:11:30 +0530

    Add files via upload

---
+++ INTRO TO JAVA B1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+      //System.out.println("jesus");
+      //System.out.println(4*4);
+      //System.out.println("hello"+""+"10");
+      //System.out.println("hello"+" "+"10");
+      //System.out.print("Hello" + "80" + "20");
+     /* Scanner s = new Scanner(System.in);
+      int a = s.nextInt();
+      int b = s.nextInt();
+      System.out.println("a = " + a);
+		  System.out.println("b = " + b);
+	   	System.out.println(a + b);
+		  System.out.println(a - b);
+		  System.out.println(a * b);*/
+   /* System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);*/
+    /*Scanner s = new Scanner(System.in);
+    String STR = s.nextLine();
+    System.out.print(STR);*/
+
+
+
+  }
+  
+
+}
